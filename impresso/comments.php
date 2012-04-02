9a33cf091a0f5d7215045dd286e287325e4bfa62
Martin Tóth <ezimir@gmail.com>
2012-04-02 11:04:38 +0200

    Original file

---
+++ impresso/comments.php
@@ -0,0 +1,56 @@
+	<div id="comments">
+	<?php if ( post_password_required() ) : ?>
+		<div class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'friendly_mercury_theme' ); ?></div>
+	</div><!-- .comments -->
+	<?php return;
+		endif;
+	?>
+
+	<?php if ( have_comments() ) : ?>
+		<h2 id="comments-title">
+			<?php
+				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'friendly_mercury_theme' ),
+					number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+			?>
+		</h2>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-above">
+			<h1 class="section-heading"><?php _e( 'Comment navigation', 'friendly_mercury_theme' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'friendly_mercury_theme' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'friendly_mercury_theme' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
+
+		<ol class="commentlist">
+			<?php wp_list_comments( array( 'callback' => 'friendly_comment_callback', 'type' => 'comment' ) ); ?>
+		</ol>
+
+		<ol class="commentlist pings">
+			<?php wp_list_comments( array( 'callback' => 'friendly_comment_callback', 'type' => 'pings' ) ); ?>
+		</ol>
+
+		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below">
+			<h1 class="section-heading"><?php _e( 'Comment navigation', 'friendly_mercury_theme' ); ?></h1>
+			<div class="nav-previous"><?php previous_comments_link( __( '&larr; Older Comments', 'friendly_mercury_theme' ) ); ?></div>
+			<div class="nav-next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'friendly_mercury_theme' ) ); ?></div>
+		</nav>
+		<?php endif; // check for comment navigation ?>
+
+	<?php else : // this is displayed if there are no comments so far ?>
+
+		<?php if ( comments_open() ) : // If comments are open, but there are no comments ?>
+
+		<?php else : if ( ! comments_open() && ! is_page() ) : ?>
+			<p class="nocomments"><?php _e( 'Comments are closed.', 'friendly_mercury_theme' ); ?></p>
+			<?php endif; // end ! comments_open() && ! is_page() ?>
+
+
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+	<?php comment_form(); ?>
+
+</div><!-- #comments -->
