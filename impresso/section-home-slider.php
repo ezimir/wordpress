15fae3595d165d3ba3fdda377c6b1acf48488eeb
Martin Tóth <ezimir@gmail.com>
2012-04-18 17:47:27 +0200

    Use images in upload directory for slider
    All images used in slider are to be stored in upload directory (most
    probably /wp-content/uploads/), because this way, admin can upload
    images through Multimedia admin panel.

--- impresso/section-home-slider.php
+++ impresso/section-home-slider.php
@@ -1,21 +1,14 @@
-<?php global $style_dir; ?>
-
 <?php
-	$w = 990;
-	$h = 375;
+	$upload_dir = wp_upload_dir();
+	$upload_url = $upload_dir['baseurl'] . '/';
 
 	// PUT IMAGE NAMES HERE
 	// use following format for line:
-	//  'filename.jpg',
+	//  'yyyy/mm/filename.jpg',
 	$slider_images = array(
-		'f1.jpg',
-		'f2.jpg',
-		'f3.jpg',
-		'f4.jpg',
-		'f5.jpg',
-		'f6.jpg',
 	);
-	// these images have to be in /images/ directory
+	// these images have to be in /wp-content/uploads/ directory
+	// or wherever is current upload directory
 ?>
 
 <?php if (count($slider_images)) : ?>
@@ -24,7 +17,7 @@
 
 	<div id="slider">
 		<?php foreach ($slider_images as $slider_image) { ?>
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/thumb.php?src=<?php echo $style_dir; ?>/images/<?php echo $slider_image; ?>&h=<?php echo $h; ?>&w=<?php echo $w; ?>&zc=1" alt="" />
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/thumb.php?src=<?php echo $upload_url . $slider_image; ?>&h=350&w=990&zc=1" alt="" />
 		<?php } ?>
 	</div>
 </div>
