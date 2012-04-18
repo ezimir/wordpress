b52731501f43e1c6ed2166c1045c69a1c875ea08
Martin Tóth <ezimir@gmail.com>
2012-04-18 17:13:34 +0200

    Added slider to front page
    Using separate file to add slider to home page. Slider only appears, if
    $slider_images variable contains any image names. All images must reside
    in theme's /images/ directory, array shall contain only file names (with
    extension).

--- impresso/front-page.php
+++ impresso/front-page.php
@@ -3,6 +3,8 @@
 
 <?php get_template_part('section','subtitles'); ?>
 
+<?php get_template_part('section', 'home-slider'); ?>
+
 <?php get_template_part('category-content'); ?>
 
 
---
+++ impresso/section-home-slider.php
@@ -0,0 +1,26 @@
+<?php global $style_dir; ?>
+
+<?php
+	$w = 990;
+	$h = 375;
+
+	// PUT IMAGE NAMES HERE
+	// use following format for line:
+	//  'filename.jpg',
+	$slider_images = array(
+	);
+	// these images have to be in /images/ directory
+?>
+
+<?php if (count($slider_images)) : ?>
+<div class="slider-wrapper theme-default">
+	<div class="ribbon"></div>
+
+	<div id="slider">
+		<?php foreach ($slider_images as $slider_image) { ?>
+		<img src="<?php echo get_stylesheet_directory_uri(); ?>/inc/thumb.php?src=<?php echo $style_dir; ?>/images/<?php echo $slider_image; ?>&h=<?php echo $h; ?>&w=<?php echo $w; ?>&zc=1" alt="" />
+		<?php } ?>
+	</div>
+</div>
+<?php endif; ?>
+
