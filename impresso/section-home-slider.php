422c4dab48e9b183a4ea8daca2ef9da7173db3aa
Martin Tóth <ezimir@gmail.com>
2012-04-19 17:37:55 +0200

    Automatic picture generation using media tags
    If MediaTags plugin is available, use pictures tagged with media tag
    'slideshow' to generate slider. If not present, fall back to
    specification by hand.

--- impresso/section-home-slider.php
+++ impresso/section-home-slider.php
@@ -2,15 +2,35 @@
 	// PUT IMAGE NAMES HERE
 	// use following format for line:
 	//  'yyyy/mm/filename.jpg',
-	$slider_images = array(
+	$images = array(
 	);
 	// these images have to be in /wp-content/uploads/ directory
 	// or wherever is current upload directory
 
-	$upload_dir = wp_upload_dir();
-	$upload_url = $upload_dir['baseurl'] . '/';
-	for ($i = 0; $i < count($slider_images); $i++) {
-		$slider_images[$i] = $upload_url . $slider_images[$i];
+	$mediatag = 'slideshow';
+	$slider_images = array();
+
+	if (function_exists('get_attachments_by_media_tags')) {
+		function sort_by_title($a, $b) {
+			if ($a->post_title == $b->post_title) return 0;
+			return ($a->post_title > $b->post_title) ? 1 : -1;
+		}
+
+		$media = get_attachments_by_media_tags('media_tags=' . $mediatag);
+		if ($media) {
+			usort($media, 'sort_by_title');
+			foreach ($media as $image) {
+				$slider_images[] = $image->guid;
+			}
+		}
+	}
+
+	if (count($slider_images) == 0 && count($images) > 0) {
+		$upload_dir = wp_upload_dir();
+		$upload_url = $upload_dir['baseurl'] . '/';
+		for ($i = 0; $i < count($images); $i++) {
+			$slider_images[$i] = $upload_url . $images[$i];
+		}
 	}
 ?>
 
