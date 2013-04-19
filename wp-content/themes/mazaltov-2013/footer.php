dded2bde8ed3515773508e3c7c9484e1f987991a
Martin Tóth <ezimir@gmail.com>
2013-04-19 17:22:42 +0200

    Adding links to footer

--- wp-content/themes/mazaltov-2013/footer.php
+++ wp-content/themes/mazaltov-2013/footer.php
@@ -8,9 +8,9 @@
     <?php wp_nav_menu( array( 'theme_location' => 'footer', 'sort_column' => 'menu_order', 'container' => false, 'menu_id' => 'menu-footer' ) ); ?>
 
     <div id="site-info">
-        <span>Arthea, s.r.o. 2012</span>,
-        <span><? _e('design', 'mazaltov'); ?> Šomšák 2012</span>,
-        <span><? _e('template', 'mazaltov'); ?> Martin Tóth 2012</span>
+        <span> <a href="http://arthea.sk">Arthea, s.r.o.</a> 2013 </span>,
+        <span> <? _e( 'design', 'mazaltov' ); ?> <a href="http://www.somsak.sk">Šomšák</a> 2013 </span>,
+        <span> <? _e( 'template', 'mazaltov' ); ?> <a href="http://lkd.to/ezimir">Tóth</a> 2013 </span>
     </div><!-- #site-info -->
 </div><!-- #footer -->
 </body>
