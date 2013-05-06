7b21c9675d5b37fd32d1e407d00a2b4da9da4d31
Martin Tóth <ezimir@gmail.com>
2013-05-06 17:20:05 +0200

    Display last link in breadcrumbs in black

--- wp-content/themes/mazaltov-2013/breadcrumbs.php
+++ wp-content/themes/mazaltov-2013/breadcrumbs.php
@@ -3,9 +3,11 @@
     <a class="subtitle-link" href="<?php bloginfo( 'url' ) ?>/"><?php _e( is_front_page() ? 'Jewish Culture Festival' : 'Mazal Tov', 'mazaltov' ); ?></a>
 
 <?php
+    $separator = ' <span class="separator">/</span> ';
+
     if ( is_page() ) {
         $post = $wp_query->get_queried_object();
-        echo ' <span class="separator">/</span> ' . the_title( '', '', false ) . ' <span class="separator">/</span> ';
+        echo $separator . the_title( '', '', false );
     } else {
         if ( is_category() ) {
             $category_id = get_cat_ID( single_cat_title( '', false ) );
@@ -15,7 +17,12 @@
         }
 
         if ( isSet( $category_id ) ) {
-            echo ' <span class="separator">/</span> ' . get_category_parents( $category_id, TRUE, ' <span class="separator">/</span> ' );
+            $parents = get_category_parents( $category_id, TRUE, $separator );
+
+            // remove last separator
+            $parents = substr( $parents, 0, strlen( $parents ) - strlen( $separator ) );
+
+            echo $separator . $parents;
         }
     }
 ?>
