b5b1a28939dbc68e7afbba3b88d29606687b8c8b
Martin Tóth <ezimir@gmail.com>
2013-05-08 13:16:34 +0200

    Use menu breadcrumbs also for post pages

--- wp-content/themes/mazaltov-2013/breadcrumbs.php
+++ wp-content/themes/mazaltov-2013/breadcrumbs.php
@@ -3,44 +3,46 @@
     <a class="subtitle-link" href="<?php bloginfo( 'url' ) ?>/"><?php _e( is_front_page() ? 'Jewish Culture Festival' : 'Mazal Tov', 'mazaltov' ); ?></a>
 
 <?php
-    $separator = ' <span class="separator">/</span> ';
+    class Only_Active_Walker_Nav_Menu extends Walker_Nav_Menu {
+        function start_el( &$output, $item, $depth, $args ) {
+            foreach ( $item->classes as $class ) {
+                if ( strpos( $class, 'current' ) !== false) {
+                    $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
+                    $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
 
-    if ( is_page() ) {
-        $post = $wp_query->get_queried_object();
-        echo $separator . the_title( '', '', false );
-    } elseif ( is_category() || is_single() ) {
-        class Only_Active_Walker_Nav_Menu extends Walker_Nav_Menu {
-            function start_el( &$output, $item, $depth, $args ) {
-                foreach ( $item->classes as $class ) {
-                    if ( strpos( $class, 'current' ) !== false) {
-                        $attributes  = ! empty( $item->attr_title ) ? ' title="'  . esc_attr( $item->attr_title ) .'"' : '';
-                        $attributes .= ! empty( $item->url )        ? ' href="'   . esc_attr( $item->url        ) .'"' : '';
+                    $output .= sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
+                        $args->before,
+                        $attributes,
+                        $args->link_before,
+                        apply_filters( 'the_title', $item->title, $item->ID ),
+                        $args->link_after,
+                        $args->after
+                    );
 
-                        $output .= sprintf( '%1$s<a%2$s>%3$s%4$s%5$s</a>%6$s',
-                            $args->before,
-                            $attributes,
-                            $args->link_before,
-                            apply_filters( 'the_title', $item->title, $item->ID ),
-                            $args->link_after,
-                            $args->after
-                        );
-
-                        break;
-                    }
+                    break;
                 }
             }
         }
+    }
+
+    $separator = ' <span class="separator">/</span> ';
+
+    if ( is_search() ) {
+        echo $separator . __('Search', 'mazaltov');
+    } else {
+        $menu_location = 'main';
+        if ( is_page() ) {
+            $menu_location = 'top';
+        }
 
         wp_nav_menu( array(
-            'theme_location' => 'main',
+            'theme_location' => $menu_location,
             'sort_column' => 'menu_order',
             'container' => false,
             'items_wrap' => '%3$s',
             'before' => $separator,
             'walker' => new Only_Active_Walker_Nav_Menu()
         ) );
-    } elseif ( is_search() ) {
-        echo $separator . __('Search', 'mazaltov');
     }
 ?>
 </div>
