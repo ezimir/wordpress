83af90fcf8a79ead614dc99e5fb95e1eae7a6051
Martin Tóth <ezimir@gmail.com>
2013-05-08 12:21:14 +0200

    Less numbers in footer

--- wp-content/themes/mazaltov-2013/footer.php
+++ wp-content/themes/mazaltov-2013/footer.php
@@ -8,9 +8,9 @@
     <?php wp_nav_menu( array( 'theme_location' => 'footer', 'sort_column' => 'menu_order', 'container' => false, 'menu_id' => 'menu-footer' ) ); ?>
 
     <div id="site-info">
-        <span> <a href="http://arthea.sk">Arthea, s.r.o.</a> 2013 </span>,
-        <span> <? _e( 'design', 'mazaltov' ); ?> <a href="http://www.somsak.sk">Šomšák</a> 2013 </span>,
-        <span> <? _e( 'template', 'mazaltov' ); ?> <a href="http://lkd.to/ezimir">Tóth</a> 2013 </span>
+        <span> 2013 - <a href="http://arthea.sk">Arthea, s.r.o.</a></span>,
+        <span> <? _e( 'design', 'mazaltov' ); ?> <a href="http://www.somsak.sk">Šomšák</a></span>,
+        <span> <? _e( 'template', 'mazaltov' ); ?> <a href="http://lkd.to/ezimir">Tóth</a></span>
     </div><!-- #site-info -->
 </div><!-- #footer -->
 </body>
